b49220385345e47be3e1ae2bd2b23bbdab45c63f
Harry Santoso <harrysantoso8@yahoo.com>
2023-08-15 14:28:17 +0700

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,6 +140,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the Application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -148,7 +152,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 
 init();
