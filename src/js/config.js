858c4c49bf9f9887257c36c7296a19a7f0a76e70
Harry Santoso <harrysantoso8@yahoo.com>
2023-08-15 14:17:55 +0700

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7766b2a1-c1ed-4eb8-afd2-9c05d4a62238';
+export const MODAL_CLOSE_SEC = 2.5;
